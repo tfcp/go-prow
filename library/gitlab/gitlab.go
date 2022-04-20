42a036ebca01c8ee6501d5a562ef27db53fcbd1e
tmaker <you@example.com>
2022-04-20 23:42:53 +0800

    debug

--- library/gitlab/gitlab.go
+++ library/gitlab/gitlab.go
@@ -2,12 +2,14 @@ package gitlab
 
 import (
 	"fmt"
+	"github.com/gogf/gf/frame/g"
 	"prow/library/log"
 	"github.com/xanzy/go-gitlab"
 )
 
 func GetGitLabClient() (*gitlab.Client, error) {
-	git, err := gitlab.NewClient(setting.GitLabSetting.Token, gitlab.WithBaseURL(setting.GitLabSetting.Url))
+	//git, err := gitlab.NewClient(setting.GitLabSetting.Token, gitlab.WithBaseURL(setting.GitLabSetting.Url))
+	git, err := gitlab.NewClient(g.Config().GetString("gitlab.token"), gitlab.WithBaseURL(g.Config().GetString("gitlab.url")))
 	if err != nil {
 		log.Logger.Error("CommentService error:", err)
 		return nil, err
